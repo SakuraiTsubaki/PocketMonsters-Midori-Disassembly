f4e131a6dc3b904a410985b17feb6dd243094b51
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 11:23:53 +0900

    Add verified Japanese charmap for restored Bank 00 text

---
+++ constants/charmap.asm
@@ -0,0 +1,35 @@
+; Minimal verified Japanese charmap used by the currently restored Bank 00 text.
+; This file expands as additional original text is recovered from the ROM.
+
+charmap "@",  $50
+charmap "　", $7F
+charmap "⋯", $75
+
+charmap "エ", $83
+charmap "ケ", $88
+charmap "コ", $89
+charmap "シ", $8B
+charmap "ソ", $8E
+charmap "ト", $93
+charmap "ナ", $94
+charmap "マ", $9D
+charmap "モ", $A1
+charmap "ラ", $A5
+charmap "レ", $A7
+charmap "ロ", $A8
+charmap "ン", $AB
+charmap "ッ", $AC
+
+charmap "き", $B7
+charmap "て", $C3
+charmap "の", $C9
+charmap "わ", $DC
+charmap "ん", $DE
+
+charmap "ー", $E3
+
+; Precomposed kana codes handled by PlaceNextChar's diacritic logic.
+charmap "ざ", $2B
+charmap "だ", $30
+charmap "パ", $40
+charmap "ポ", $43
