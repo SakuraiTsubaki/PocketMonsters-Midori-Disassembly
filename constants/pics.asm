5f4c22207ca222615b54aa38e2e31e43467baa08
SakuraiTsubaki <nokang3@naver.com>
2026-09-14 01:36:07 +0900

    Add sprite postprocess constants

--- constants/pics.asm
+++ constants/pics.asm
@@ -1,4 +1,4 @@
-; Generation I Pokemon picture-bank layout used by home/pics.asm.
+; Generation I Pokemon picture-bank and sprite-buffer layout used by home/pics.asm.
 
 DEF MON_FRONT_SPRITE_PTR_OFFSET EQU $0B
 DEF MON_BACK_SPRITE_PTR_OFFSET EQU $0D
@@ -15,3 +15,13 @@ DEF PICS_1_END EQU $1F
 DEF PICS_2_END EQU $4A
 DEF PICS_3_END EQU $75
 DEF PICS_4_END EQU $9A
+
+DEF sSpriteBuffer0 EQU $A000
+DEF TILE_1BPP_SIZE EQU 8
+DEF PIC_SIZE EQU 7 * 7
+
+; These HRAM temporaries intentionally overlap other unions.
+DEF hSpriteInterlaceCounter EQU $FF8B
+DEF hSpriteWidth EQU $FF8B
+DEF hSpriteHeight EQU $FF8C
+DEF hSpriteOffset EQU $FF8D
