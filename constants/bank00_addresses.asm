d5b6c3cfc65114f1d1427649c91e8f8bb8f47c2f
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 11:11:38 +0900

    Add verified Bank 00 bootstrap addresses

---
+++ constants/bank00_addresses.asm
@@ -0,0 +1,7 @@
+; Verified fixed ROM0 targets from the uploaded Pocket Monsters Midori ROMs.
+; These are temporary address constants until each target is restored as a label.
+
+DEF BANK00_VBLANK_ADDR EQU $0AAC
+DEF BANK00_SERIAL_ADDR EQU $0BA7
+DEF BANK00_TIMER_ADDR  EQU $0D9A
+DEF BANK00_INIT_ADDR   EQU $09DA
