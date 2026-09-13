ddd34ec165791fc2d165f6ee180e5ff3b04dc56e
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 11:40:51 +0900

    Add initialization and VBlank RAM symbols

--- constants/ram.asm
+++ constants/ram.asm
@@ -1,4 +1,4 @@
-; RAM/HRAM addresses verified from the restored Bank 00 code.
+; RAM/HRAM addresses verified from restored Bank 00 instructions.
 
 DEF wShadowOAM EQU $C300
 DEF wShadowOAMSize EQU $A0
@@ -6,16 +6,31 @@ DEF wTileMap EQU $C3A0
 DEF wTextDest EQU $CC3A
 DEF wRedrawRowOrColumnSrcTiles EQU $CBFC
 DEF wBuffer EQU $CEE4
+DEF wAudioFadeOutControl EQU $CFAE
+DEF wUpdateSpritesEnabled EQU $CFB2
+DEF wLastMusicSoundID EQU $CFB1
 DEF wEnemyMonNick EQU $CFC1
 DEF wBattleMonNick EQU $CFF0
+DEF wNewSoundID EQU $C0EE
+DEF wAudioROMBank EQU $C0EF
+DEF wAudioSavedROMBank EQU $C0F0
 DEF wMovingBGTilesCounter2 EQU $D062
+DEF wDisableVBlankWYUpdate EQU $D07D
+DEF wVBlankSavedROMBank EQU $D0E7
 DEF wLinkState EQU $D0F0
 DEF wPlayerName EQU $D11D
 DEF wLetterPrintingDelayFlags EQU $D2D7
 DEF wRivalName EQU $D2CE
+DEF wStack EQU $DFFF
 
+DEF hDMARoutine EQU $FF80
+DEF hSoftReset EQU $FF8A
 DEF hROMBankTemp EQU $FF8B
 DEF hTextID EQU $FF8C
+DEF hSerialConnectionStatus EQU $FFAA
+DEF hSCX EQU $FFAE
+DEF hSCY EQU $FFAF
+DEF hWY EQU $FFB0
 DEF hJoyHeld EQU $FFB4
 DEF hJoy5 EQU $FFB5
 DEF hLoadedROMBank EQU $FFB8
@@ -36,6 +51,8 @@ DEF hVBlankCopyDoubleDest EQU $FFCE
 
 DEF hRedrawRowOrColumnMode EQU $FFD0
 DEF hRedrawRowOrColumnDest EQU $FFD1
+DEF hFrameCounter EQU $FFD5
+DEF hVBlankOccurred EQU $FFD6
 DEF hTileAnimations EQU $FFD7
 DEF hMovingBGTilesCounter1 EQU $FFD8
 DEF hWhoseTurn EQU $FFF3
