0741239f61efaed77b4b2c2b9d51ae7eb1bd7245
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 19:45:19 +0900

    Add overworld collision symbols

---
+++ constants/overworld_collision.asm
@@ -0,0 +1,9 @@
+; Land collision symbols verified from Bank 00.
+
+DEF wSimulatedJoypadStatesIndex EQU $CD38
+DEF wSpritePlayerStateData1CollisionData EQU $C10C
+DEF wTileInFrontOfPlayer EQU $CFAD
+DEF wTilesetCollisionPtr EQU $D4AF
+
+DEF SFX_COLLISION EQU $B4
+DEF BANK06_HANDLE_LEDGES_ADDR EQU $7F2A
