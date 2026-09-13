b6d0ec53064de963da72fd2b03be2d3f6448bd38
SakuraiTsubaki <nokang3@naver.com>
2026-09-14 01:33:47 +0900

    Add Pokemon picture bank constants

---
+++ constants/pics.asm
@@ -0,0 +1,17 @@
+; Generation I Pokemon picture-bank layout used by home/pics.asm.
+
+DEF MON_FRONT_SPRITE_PTR_OFFSET EQU $0B
+DEF MON_BACK_SPRITE_PTR_OFFSET EQU $0D
+
+DEF PICS_BANK_MEW EQU $01
+DEF PICS_BANK_1 EQU $09
+DEF PICS_BANK_2 EQU $0A
+DEF PICS_BANK_3 EQU $0B
+DEF PICS_BANK_4 EQU $0C
+DEF PICS_BANK_5 EQU $0D
+
+; Exclusive internal-species boundaries for the five regular picture banks.
+DEF PICS_1_END EQU $1F
+DEF PICS_2_END EQU $4A
+DEF PICS_3_END EQU $75
+DEF PICS_4_END EQU $9A
