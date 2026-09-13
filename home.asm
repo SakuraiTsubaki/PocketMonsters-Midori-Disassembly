79de46652e58b9d356ed66a042f1a4d98678336d
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 11:15:28 +0900

    Wire restored Bank 00 core into home source

--- home.asm
+++ home.asm
@@ -1,3 +1,14 @@
 INCLUDE "constants/bank00_addresses.asm"
+INCLUDE "constants/hardware.asm"
+INCLUDE "constants/ram.asm"
+
 INCLUDE "home/header.asm"
 INCLUDE "home/start.asm"
+
+SECTION "Home Core", ROM0[$0153]
+
+INCLUDE "home/joypad.asm"
+INCLUDE "home/lcd.asm"
+INCLUDE "home/clear_sprites.asm"
+INCLUDE "home/copy.asm"
+INCLUDE "data/tilesets/collision_tile_ids.asm"
