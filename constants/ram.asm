0057fce02d0e9025501d6b327234201219cad2f0
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 11:24:03 +0900

    Add RAM symbols for restored text name handlers

--- constants/ram.asm
+++ constants/ram.asm
@@ -4,8 +4,13 @@ DEF wShadowOAM EQU $C300
 DEF wShadowOAMSize EQU $A0
 DEF wTileMap EQU $C3A0
 DEF wBuffer EQU $CEE4
+DEF wEnemyMonNick EQU $CFC1
+DEF wBattleMonNick EQU $CFF0
+DEF wPlayerName EQU $D11D
+DEF wRivalName EQU $D2CE
 
 DEF hROMBankTemp EQU $FF8B
+DEF hTextID EQU $FF8C
 DEF hJoyHeld EQU $FFB4
 DEF hJoy5 EQU $FFB5
 DEF hLoadedROMBank EQU $FFB8
@@ -20,6 +25,7 @@ DEF hVBlankCopyDest EQU $FFC9
 DEF hVBlankCopyDoubleSize EQU $FFCB
 DEF hVBlankCopyDoubleSource EQU $FFCC
 DEF hVBlankCopyDoubleDest EQU $FFCE
+DEF hWhoseTurn EQU $FFF3
 
 DEF SHADOW_OAM_ENTRY_SIZE EQU 4
 DEF SHADOW_OAM_COUNT EQU 40
