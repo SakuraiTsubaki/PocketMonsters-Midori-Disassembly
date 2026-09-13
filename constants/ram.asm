829c6782d757e0b86c17e2c9ea6aa3b1345a832e
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 11:17:20 +0900

    Add verified RAM and HRAM symbols for Bank 00 copy helpers

--- constants/ram.asm
+++ constants/ram.asm
@@ -2,8 +2,24 @@
 
 DEF wShadowOAM EQU $C300
 DEF wShadowOAMSize EQU $A0
+DEF wTileMap EQU $C3A0
 DEF wBuffer EQU $CEE4
+
+DEF hROMBankTemp EQU $FF8B
+DEF hJoyHeld EQU $FFB4
+DEF hJoy5 EQU $FFB5
 DEF hLoadedROMBank EQU $FFB8
+DEF hAutoBGTransferEnabled EQU $FFBA
+
+DEF hVBlankCopyBGSource EQU $FFC1
+DEF hVBlankCopyBGDest EQU $FFC3
+DEF hVBlankCopyBGNumRows EQU $FFC5
+DEF hVBlankCopySize EQU $FFC6
+DEF hVBlankCopySource EQU $FFC7
+DEF hVBlankCopyDest EQU $FFC9
+DEF hVBlankCopyDoubleSize EQU $FFCB
+DEF hVBlankCopyDoubleSource EQU $FFCC
+DEF hVBlankCopyDoubleDest EQU $FFCE
 
 DEF SHADOW_OAM_ENTRY_SIZE EQU 4
 DEF SHADOW_OAM_COUNT EQU 40
