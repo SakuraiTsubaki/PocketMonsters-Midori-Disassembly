24881f4464e1de2b645ab906372d3db21e4aee63
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 11:18:17 +0900

    Wire restored Bank 00 copy2 block into home source

--- home.asm
+++ home.asm
@@ -1,6 +1,8 @@
 INCLUDE "constants/bank00_addresses.asm"
 INCLUDE "constants/hardware.asm"
 INCLUDE "constants/ram.asm"
+INCLUDE "constants/display.asm"
+INCLUDE "constants/input.asm"
 
 INCLUDE "home/header.asm"
 INCLUDE "home/start.asm"
@@ -12,3 +14,4 @@ INCLUDE "home/lcd.asm"
 INCLUDE "home/clear_sprites.asm"
 INCLUDE "home/copy.asm"
 INCLUDE "data/tilesets/collision_tile_ids.asm"
+INCLUDE "home/copy2.asm"
