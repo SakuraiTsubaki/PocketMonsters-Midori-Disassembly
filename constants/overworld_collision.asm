6d88a3c0dce1a262ac0b84f519539149d03ac882
SakuraiTsubaki <nokang3@naver.com>
2026-09-15 14:39:45 +0900

    Reset repository working tree for clean restart

--- constants/overworld_collision.asm
+++
@@ -1,8 +0,0 @@
-; Land collision symbols verified from Bank 00.
-
-DEF wSpritePlayerStateData1CollisionData EQU $C10C
-DEF wTileInFrontOfPlayer EQU $CFAD
-DEF wTilesetCollisionPtr EQU $D4AF
-
-DEF SFX_COLLISION EQU $B4
-DEF BANK06_HANDLE_LEDGES_ADDR EQU $7F2A
